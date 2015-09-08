d06ed3db0b11050aa25fbb17a9cd3c15752c239b
William Enders <14hourlunchbreak@gmail.com>
2015-09-08 12:27:16 -0400

    door!

---
+++ PoeticLandscape/Assets/openDoor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class openDoor : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(){
+		GameObject.Find ("door").GetComponent<MeshRenderer> ().enabled = false;
+		GameObject.Find ("door").GetComponent<BoxCollider> ().enabled = false;
+		GameObject.Find ("door").GetComponent<AudioSource> ().Play ();
+	}
+	void OnTriggerExit(){
+		GameObject.Find ("door").GetComponent<MeshRenderer> ().enabled = true;
+		GameObject.Find ("door").GetComponent<BoxCollider> ().enabled = true;
+	}
+
+}
